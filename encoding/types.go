d97651501c3c431db8659680eea41379be8319f2
DNahar74 <nahardarsh55@gmail.com>
2025-07-02 16:40:47 +0530

    added a default decoder and a basic encoding struct + methods

--- encoding/types.go
+++ encoding/types.go
@@ -2,15 +2,44 @@ package encoding
 
 import (
 	"encoding/gob"
+	"fmt"
 	"io"
+
+	"github.com/DNahar74/distributed-file-system/message"
 )
 
 type Decoder interface {
-	Decode(io.Reader, any) error
+	Decode(io.Reader, *message.Message) error
 }
 
-type GOBDecoder struct {}
+type GOBDecoder struct{}
 
-func (dec GOBDecoder) Decode(r io.Reader, v any) error {
-	return gob.NewDecoder(r).Decode(v)
+func (dec GOBDecoder) Decode(r io.Reader, msg *message.Message) error {
+	fmt.Println("Hello from GOB decoder")
+	err := gob.NewDecoder(r).Decode(msg)
+	return err
+}
+
+type NOPDecoder struct{}
+
+func (dec NOPDecoder) Decode(r io.Reader, msg *message.Message) error {
+	buf := make([]byte, 4096)
+	n, err := r.Read(buf)
+	if err != nil {
+		return err
+	}
+	msg.Payload = buf[:n]
+	return nil
+}
+
+type Encoder interface {
+	Encode(io.Reader, any) error
+}
+
+type GOBEncoder struct{}
+
+func (enc GOBEncoder) Encode(r io.Writer, v any) error {
+	fmt.Println("Hello from Encoder")
+	err := gob.NewEncoder(r).Encode(v)
+	return err
 }
